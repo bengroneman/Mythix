38062065445d1f368820f4f6b67df89c8499acfd
bengroneman <bluegroneman10@gmail.com>
2020-05-06 09:11:12 -0600

    inital checkin

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    public float runSpeed = 40f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+    // This function is best used for physics
+    void FixedUpdate()
+    {
+        // No matter how many times this function is called we will still be moving at the same speed
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
